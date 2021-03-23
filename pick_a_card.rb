743c416d0c9a9e8bd177aead84e023f6fce7e97d
alibek <bajmuhametovalibek30@gmail.com>
2021-03-23 20:30:03 +0300

    Multiple cards

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -3,6 +3,21 @@ values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 # Создадим массив с набором мастей.
 # Буби, черви, крести, пики.
 suits = ["♦ ", "♠ ", "♥ ", "♣ "]
+ 
+cards = []
 
-puts "#{values.sample} of #{suits.sample}"
+values.each do |value|
+  suits.each do |suit|
+      cards << "#{value} of #{suit}"
+  end
+end
 
+card.shuffle!
+
+puts 'How many cards do you pick'
+
+number = STDIN.gets.to_i
+
+number.times do 
+  puts cards.pop
+end  
